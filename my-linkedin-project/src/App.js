8cd132d4e068bed60d3f114ea2352dccb1818436
lissms <felissms@gmail.com>
2021-03-26 16:19:37 +0100

    Create store and reducer

--- my-linkedin-project/src/App.js
+++ my-linkedin-project/src/App.js
@@ -1,11 +1,33 @@
-import ButtonSurveying from "./surveging/ButtonSurveying";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 import SurveyingLanding from "./surveging/SurveyingLanding";
+
+const initialState = {
+  title: "",
+};
+
+// Descripción de como se cambiara el estado global
+// configuración
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "CHANGE_TITLE":
+      return { ...state, title: action.payload };
+    default:
+      return state;
+  }
+};
+
+const reduxChromeExtensionSettings = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(reducer, reduxChromeExtensionSettings);
 
 function App() {
   return (
     <div className="App">
-      <SurveyingLanding></SurveyingLanding>
+      <Provider store={store}>
+        <SurveyingLanding />
+      </Provider>
     </div>
   );
 }
