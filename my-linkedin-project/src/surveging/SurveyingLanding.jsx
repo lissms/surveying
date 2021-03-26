b1378e880ad7c7bf780b3e7aa6663c83e3ed608d
lissms <felissms@gmail.com>
2021-03-26 16:21:01 +0100

    Link redux to SurveingLanding component

--- my-linkedin-project/src/surveging/SurveyingLanding.jsx
+++ my-linkedin-project/src/surveging/SurveyingLanding.jsx
@@ -3,9 +3,10 @@ import SurveyingTitle from "./SurveyingTitle";
 import { Header, LandingContainer, SurveyContainer } from "./surveyingLanding.styled";
 import Options from "./Options";
 import SurveyingFooter from "./SurveyingFooter";
-import MenuContent from "../menu/Menu";
+import Menu from "../menu/Menu";
+import { connect } from "react-redux";
 
-function SurveyingLanding() {
+function SurveyingLanding(props) {
   const options = [
     {
       name: "redux",
@@ -35,20 +36,26 @@ function SurveyingLanding() {
 
   return (
     <>
-      {/* <LandingContainer>
-      <Header>
-        <h1>Surveying</h1>
-      </Header>
-      <SurveyContainer>
-        <SurveyingTitle title="What is the best way to manage the state in React"></SurveyingTitle>
-        <Options options={options}></Options>
-      </SurveyContainer>
-      <SurveyingFooter></SurveyingFooter>
-    </LandingContainer> */}
+      <LandingContainer>
+        <Header>
+          <h1>Surveying</h1>
+        </Header>
+        <SurveyContainer>
+          <SurveyingTitle
+            title={props.titleProp || "What is the best way to manage the state in React"}
+          ></SurveyingTitle>
+          <Options options={options}></Options>
+        </SurveyContainer>
+        <SurveyingFooter></SurveyingFooter>
+      </LandingContainer>
 
-      <MenuContent></MenuContent>
+      <Menu></Menu>
     </>
   );
 }
 
-export default SurveyingLanding;
+const mapStateToProps = (state) => ({
+  titleProp: state.title,
+});
+
+export default connect(mapStateToProps, null)(SurveyingLanding);
